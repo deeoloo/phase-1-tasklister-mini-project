f6f7a0ad1ecf5d49f6b202cbd85648c8866d43fa
deenoloo <doreenoloo2@gmail.com>
2025-03-17 15:58:35 -0400

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById('create-task-form')
-  const taskList = document.getElementById('tasks')
-  form.addEventListener('submit',(e)=>{
-  e.preventDefault()
-  const taskDescription = document.getElementById("new-task-description").value;
-  
-  const taskItem = document.createElement("li");
-  taskItem.textContent = taskDescription;
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
 
-  taskList.appendChild(taskItem);
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const taskDescription = document.getElementById("new-task-description").value;
 
-  form.reset();})
-  // your code here
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskDescription;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => {
+      taskItem.remove();
+    });
+
+    taskItem.appendChild(deleteButton);
+
+    taskList.appendChild(taskItem);
+
+    form.reset();
+  });
 });
-
-

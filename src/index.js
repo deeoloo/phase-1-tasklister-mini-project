e7c37b8c9f1072a07a1825867d0fc28c2f80c428
deenoloo <doreenoloo2@gmail.com>
2025-03-17 15:48:39 -0400

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById('create-task-form')
+  const taskList = document.getElementById('tasks')
+  form.addEventListener('submit',(e)=>{
+  e.preventDefault()
+  const taskDescription = document.getElementById("new-task-description").value;
+  
+  const taskItem = document.createElement("li");
+  taskItem.textContent = taskDescription;
+
+  taskList.appendChild(taskItem);
+
+  form.reset();})
   // your code here
 });
+
+
